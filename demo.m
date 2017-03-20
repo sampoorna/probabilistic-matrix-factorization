5514d1875ddcaa251acaf710c905d15e65ae1b40
Sampoorna <sambis@cs.ubc.ca>
2017-03-20 13:49:12 -0700

    Fixed a small bug, added dataset URL

--- demo.m
+++ demo.m
@@ -1,6 +1,6 @@
 restart=1;
 NUM_FACTORS = 20; % Dimension of latent vectors
-origdirec = '/ml-1m/'; 
+origdirec = 'ml-1m/'; 
 fprintf(1,'Running Probabilistic Matrix Factorization (PMF) \n');
 load(strcat(origdirec, 'data_withoutrat_randcold2.mat'));
 MAX_USERS = max(warm(:, 2));
