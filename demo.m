7bfc74dc03c839250006c29cca57db9dbf9f5703
Sampoorna <sambis@cs.ubc.ca>
2017-03-20 12:16:52 -0700

    Added Comments

--- demo.m
+++ demo.m
@@ -1,9 +1,9 @@
 restart=1;
-MAX_USERS = 6040;
-MAX_ITEMS = 3952;
-NUM_FACTORS = 20;
+NUM_FACTORS = 20; % Dimension of latent vectors
 fprintf(1,'Running Probabilistic Matrix Factorization (PMF) \n');
 load '/ml-1m/data_withoutrat_randcold2.mat';
+MAX_USERS = max(warm(:, 1));
+MAX_ITEMS = max(warm(:, 2));
 pmf_modified
 
 restart=1;
